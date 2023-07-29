bb38b9bd2a974db13ae8b3f658da1e9aebf2a25f
mtalli <mauno.talli@hotmail.com>
2023-07-29 08:05:49 +0300

    First version without RGB

--- script.js
+++ script.js
@@ -9,3 +9,23 @@ function createGrid(size) {
 }
 
 createGrid(16);
+
+const gridItems = document.querySelectorAll('.grid-item');
+gridItems.forEach(gridItem => {
+    gridItem.addEventListener('mouseenter', function() {
+        this.style.backgroundColor = getRandomColor();
+    });
+
+    gridItem.addEventListener('mouseLeave', function() {
+        this.style.backgroundColor = '';
+    })
+})
+
+function getRandomColor() {
+    const letter = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letter[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
